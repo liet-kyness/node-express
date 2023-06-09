66762d58c49aaedc2af78e27690842250f3c5325
lietkynes <mdhill387@gmail.com>
2023-06-08 22:23:41 -0400

    second commit

--- urls.js
+++ urls.js
@@ -6,17 +6,17 @@ const axios = require('axios');
 const URLS = [];
 const path = process.argv[2];
 
-function writeFile(path, data) {
-    let strPath = path.toString();
-    let strData = data.toString();
-    fs.writeFile(strPath, strData, 'utf8', (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(`wrote file to ${path}`);
-        };
-    });
-};
+// function writeFile(path, data) {
+//     let strPath = path.toString();
+//     let strData = data.toString();
+//     fs.writeFile(strPath, strData, 'utf8', (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(`wrote file to ${path}`);
+//         };
+//     });
+// };
 
 function cat(path, err) {
     readline.createInterface({
@@ -28,15 +28,23 @@ function cat(path, err) {
 
        
         //let urls = URLS;
-        let reqData = URLS.forEach(async (u) => {
-        return await axios.get(u.data);
+        let resData = URLS.map(async (u) => {
+        return await axios.get(u.data).toString();
        });
-       console.log(reqData);
-       let pathName = URLS.forEach(async (r) => {
-        return await axios.get(r.hostname);
+       console.log(resData);
+       let pathName = URLS.map(async (u) => {
+        return await axios.get(u.hostname).toString();
        });
        console.log(pathName);
-       return URLS.forEach(writeFile(pathName, reqData));
+       URLS.forEach(async pathName => {
+        await fs.writeFile(pathName, resData.toString(), (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`wrote to ${pathName}`);
+            }
+        });
+       });
 }); 
 }     
 
